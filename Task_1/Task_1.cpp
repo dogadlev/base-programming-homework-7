167d9f47e69eacce82f03c1236b1aebcb4464379
Andrei Dogadaev <dogadlev@yandex.ru>
2022-07-28 20:30:15 +0300

    Initial commit

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,37 @@
+﻿// Homework 7. Task 1.
+
+#include <iostream>
+#include <clocale>
+
+#define MODE 1
+
+#ifndef MODE
+#error Ошибка! Необходимо определить MODE.
+#endif
+
+#if MODE == 1
+int add(int num1, int num2)
+{
+    return (num1 + num2);
+}
+#endif
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int num1 = 0;
+    std::cout << "Введите число 2: ";
+    std::cin >> num1;
+    int num2 = 0;
+    std::cout << "Введите число 2: ";
+    std::cin >> num2;
+    std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+return 0;
+}
---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,16 @@
+﻿// Homework 7. Task 2.
+
+#include <iostream>
+
+#define SUB(a,b) ((a) - (b))
+
+int main(int argc, char** argv)
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+	return 0;
+}
